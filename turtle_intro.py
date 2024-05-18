6880a17420164cfd99c0539c6947d4f091fedf90
anoushka1111 <saha.anoushka@gmail.com>
2024-05-18 19:19:22 -0400

    draw a square

---
+++ turtle_intro.py
@@ -0,0 +1,18 @@
+#Anoushka Saha
+#Drawing a square
+
+#May 18th, 2024
+#Day 18 Practice
+
+from turtle import Turtle, Screen
+name = Turtle()
+name.forward(100)
+name.right(90)
+name.forward(100)
+name.right(90)
+name.forward(100)
+name.right(90)
+name.forward(100)
+
+screen = Screen()
+screen.exitonclick()
